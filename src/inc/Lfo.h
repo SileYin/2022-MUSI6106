0ef938977580c9c5010667ab5a19065d041cd11b
Sile Yin <42690744+SileYin@users.noreply.github.com>
2022-03-02 18:34:36 -0500

    Add precision to lfo increment, now works better!

--- src/inc/Lfo.h
+++ src/inc/Lfo.h
@@ -9,7 +9,7 @@ class CWavetableLFO
 public:
     CWavetableLFO(int iWavetableLength, int iSampleRate, float fFrequency, float fAmplitude):
         m_iSampleRate(iSampleRate),
-        m_fReadIdx(0.f)
+        m_dReadIdx(0)
     {
         double const l_dPi = 3.14159265358979323846;
         assert(iWavetableLength > 0);
@@ -19,7 +19,7 @@ public:
             pCWavetableRingBuff->putPostInc(sin(2 * l_dPi * i / iWavetableLength));
         }
         m_fAmplitude = fAmplitude; 
-        m_fReadInc = static_cast<float>(pCWavetableRingBuff->getLength()) / static_cast<float>(m_iSampleRate) * fFrequency;
+        m_dReadInc = static_cast<float>(pCWavetableRingBuff->getLength()) / static_cast<float>(m_iSampleRate) * fFrequency;
     }
     ~CWavetableLFO()
     {
@@ -28,7 +28,7 @@ public:
     }
     void setReadPhase(float fPhaseInDegrees)
     {
-        m_fReadIdx = fPhaseInDegrees / 360 * static_cast<float>(pCWavetableRingBuff->getLength());
+        m_dReadIdx = fPhaseInDegrees / 360 * static_cast<float>(pCWavetableRingBuff->getLength());
     }
     int getWavetableLength() const
     {
@@ -36,12 +36,12 @@ public:
     }
     void setLFOFrequency(float fFrequency, float fPhaseInDegrees = (0.f))
     {
-        m_fReadInc = static_cast<float>(pCWavetableRingBuff->getLength()) / static_cast<float>(m_iSampleRate) * fFrequency;
+        m_dReadInc = static_cast<float>(pCWavetableRingBuff->getLength()) / static_cast<float>(m_iSampleRate) * fFrequency;
         setReadPhase(fPhaseInDegrees);
     }
     int getLFOFrequency() const
     {
-        return m_fReadInc * static_cast<float>(m_iSampleRate) / static_cast<float>(pCWavetableRingBuff->getLength());
+        return m_dReadInc * static_cast<float>(m_iSampleRate) / static_cast<float>(pCWavetableRingBuff->getLength());
     }
     void setLFOAmplitude(float fAmplitude)
     {
@@ -54,20 +54,20 @@ public:
     float getPostInc()
     {
         float l_fCurrentValue;
-        l_fCurrentValue = m_fAmplitude * pCWavetableRingBuff->get(m_fReadIdx);
-        m_fReadIdx += m_fReadInc;
-        while (m_fReadIdx > pCWavetableRingBuff->getLength())
+        l_fCurrentValue = m_fAmplitude * pCWavetableRingBuff->get(m_dReadIdx);
+        m_dReadIdx += m_dReadInc;
+        while (m_dReadIdx > pCWavetableRingBuff->getLength())
         {
-            m_fReadIdx = m_fReadIdx - pCWavetableRingBuff->getLength();
+            m_dReadIdx = m_dReadIdx - pCWavetableRingBuff->getLength();
         }
         return l_fCurrentValue;
     }
 private:
     CRingBuffer<float>* pCWavetableRingBuff = 0;
     int m_iSampleRate;
-    float m_fReadIdx;
+    double m_dReadIdx;
     float m_fAmplitude;
-    float m_fReadInc;
+    double m_dReadInc;
 };
 
 
